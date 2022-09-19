26dc143b185c57beac181cef8ac6841db9ae416f
L3on06 <l3onshabani@gmail.com>
2022-09-18 20:46:43 -0700

    Adding search,responsive bar,movie card,background

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ menu.addEventListener("click", () => {
     menu.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
+        
+var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 5,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            loopFillGroupWithBlank: true,
+              autoplay: {
+                    delay:3500,
+                    disableOnInteraction: false,
+        },
+    navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            }
+        });
